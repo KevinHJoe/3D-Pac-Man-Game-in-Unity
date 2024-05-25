8682300481c89d3f81792e69ea10018fa3c31465
Kevin <KevinHJoe@gmail.com>
2024-05-25 12:39:44 -0700

    First Commit

---
+++ Assets/Scripts/PathfinderWaypoints.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathfinderWaypoints : MonoBehaviour
+{
+
+    //23 side       //row
+    //19 top        //collum
+
+    GameObject[][] waypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoint = new GameObject[23][];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //each ghost has a seperate area of the map that they will occasionally circle around
+    public void ghost1()
+    {
+
+    }
+
+    public void ghost2()
+    {
+
+    }
+
+    public void ghost3()
+    {
+
+    }
+
+    public void ghost4()
+    {
+
+    }
+}
